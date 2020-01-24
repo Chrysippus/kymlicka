98db646c133d48c06ea3b20b8b86be58bf0c09e5
Chrysippus <torin.doppelt@gmail.com>
2020-01-24 06:14:22 -0500

    Minor changes to navbar.

--- src/components/shared/WKNavbar.js
+++ src/components/shared/WKNavbar.js
@@ -14,26 +14,26 @@ export default function WKNavbar() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" className="border-0" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mx-auto">
-          <Nav.Link as={Link} to="/">
+          <Nav.Link eventKey={1} as={Link} to="/">
             Home
           </Nav.Link>
-          <Nav.Link as={Link} to="/biography">
+          <Nav.Link eventKey={2} as={Link} to="/biography">
             Biography
           </Nav.Link>
-          <Nav.Link as={Link} to="/cv">
+          <Nav.Link eventKey={3} as={Link} to="/cv">
             CV
           </Nav.Link>
-          <Nav.Link as={Link} to="/publications">
+          <Nav.Link eventKey={4} as={Link} to="/publications">
             Publications
           </Nav.Link>
-          <Nav.Link as={Link} to="/affiliations">
+          <Nav.Link eventKey={5} as={Link} to="/affiliations">
             Affiliations
           </Nav.Link>
-          <Nav.Link as={Link} to="/newsletter">
+          <Nav.Link eventKey={6} as={Link} to="/newsletter">
             Newsletter
           </Nav.Link>
-          <Nav.Link href="https://www.queensu.ca/mcp">MCP Index</Nav.Link>
-          <Nav.Link as={Link} to="/animal-studies-fellowship">
+          <Nav.Link eventKey={7} href="https://www.queensu.ca/mcp">MCP Index</Nav.Link>
+          <Nav.Link eventKey={8} as={Link} to="/animal-studies-fellowship">
             Animal Studies Fellowship
           </Nav.Link>
         </Nav>
